8efe23f3f36a3da3b4358e7c06d4eba135e00277
JedrychowskiEmil <38006625+JedrychowskiEmil@users.noreply.github.com>
2020-11-24 11:23:32 +0100

    v0.3a
    Dodano działający przykład

--- Main.java
+++ Main.java
@@ -16,11 +16,14 @@ class Main {
         String lookingFor = "    <link href=\"files/global.css\" type=\"text/css\" rel=\"stylesheet\"/>";
         String lookingForFlag = "    <style type=\"text/css\">";
 
+        String lookingFor2 = "\t<script type=\"text/javascript\" src=\"config_resource/jszip.js\"></script><script type=\"text/javascript\" src=\"config_resource/xlsx.js\"></script><script type=\"text/javascript\" src=\"config_resource/script.js\"></script>";
+        String lookingForFlag2 = "    <script src=\"resources/scripts/axure/ios.js\"></script>";
+
         FileCSSFixer fileCSSFixer = new FileCSSFixer(file, path2);
         File tmpFile = fileCSSFixer.pasteIfMissing(lookingFor, lookingForFlag);
         System.out.println(tmpFile.getAbsolutePath());
         FileCSSFixer fileCSSFixer2 = new FileCSSFixer(tmpFile, path);
-        File tmpFile2 = fileCSSFixer2.pasteIfMissing(lookingFor, lookingForFlag);
+        File tmpFile2 = fileCSSFixer2.pasteIfMissing(lookingFor2, lookingForFlag2);
         System.out.println(tmpFile2.getAbsolutePath());
     }
 }
